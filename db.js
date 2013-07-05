54129dd4867741412fab95eb6568824adea88779
Taylor <trflagg@gmail.com>
2013-07-04 23:30:17 -0400

    Add test skeleton. Better db connection.

--- db.js
+++ db.js
@@ -5,4 +5,9 @@ var environment,
 module.exports.init = function(env) {
     _environment = env;
     _db = mongo.db(env.db.URL);
+    return _db;
 };
+
+module.exports.db = function() {
+    return _db;
+}
---
+++ tests/runTests.js
@@ -0,0 +1,31 @@
+/**
+ * Create connection to testdb and run all tests.
+ */
+module.exports.runTests = function() {
+
+    var assert = require('assert'),
+         async = require('async');
+
+
+    console.log('\n________ Running All Tests ____________');
+
+    // load service based on environment
+    var environment = require('../environments/environment-test');
+    var db = require('../db').init(environment);
+
+    async.series([
+            function(callback) {
+                require('./databaseInit')(db, callback);
+            },
+            function(callback) {
+                console.log('');
+                callback();
+            }
+        ],
+        function(err, results) {
+            console.log('________ Tests Ended __________________');
+            console.log('');
+            db.close();
+        }
+    );
+}();

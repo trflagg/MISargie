39d35a379996873b3a817d6b797f74ca024e0687
Taylor <trflagg@gmail.com>
2013-07-24 21:54:12 -0400

    client loads multi-message files.

--- loadFixtures.js
+++ loadFixtures.js
@@ -10,6 +10,15 @@ module.exports = function() {
         fileEmitter = new EventEmitter(),
         messageList = [];
 
+    var saveMessage = function(messageName, messageText) {
+        var newMessage = db.create('Message');
+        newMessage.setName(messageName);
+        newMessage.setText(messageText);
+        newMessage.compile();
+        db.save('Message', newMessage);
+        // console.log(messageName);
+    }
+
     // remove all to begin
     db.deleteAll('Message');
 
@@ -17,14 +26,41 @@ module.exports = function() {
     fs.readdirSync('./fixtures').forEach(function(file) {
         console.log(file);
         var data = fs.readFileSync('./fixtures/' + file, {encoding: 'utf8'})
-        var filenameParse = /(\w+)\.\w+/.exec(file);
-        console.log(data);
 
-        var newMessage = db.create('Message');
-        newMessage.setName(filenameParse);
-        newMessage.setText(data);
-        newMessage.compile();
-        db.save('Message', newMessage);
+        // check for multi-message file.
+        // starts with # [messageName]
+        if (data[0] === "#") {
+            lines = data.split('\n');
+            // take off '# '
+            var messageName = /^# (\w+)/.exec(lines[0])[1];
+
+            // read line by line
+            var text = '';
+            for (var i=1, ll=lines.length; i < ll; i++) {
+                var line = lines[i];
+
+                if (line[0] === "#") {
+                    if (line == '#--') {
+                        // new message
+                        saveMessage(messageName, text);
+                        text = '';
+                        i++;
+                        var messageName = /^# (\w+)/.exec(lines[i])[1];
+                    }
+                } else {
+                    text = text + '\n' + line;
+                }
+            }
+
+            saveMessage(messageName, text);
+        }
+        else {
+            // take off extension
+            var messageName = /(\w+)\.\w+/.exec(file)[1];
+            var messageText = data;
+            saveMessage(messageName, messageText);
+        }
+
         console.log('saved ----------------------');
     });
     // done.

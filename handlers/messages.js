2feb4a7bac56b4bdff6709c56ccef2ca85c97f08
Taylor Flagg <trflagg@gmail.com>
2018-08-28 16:44:52 -0400

    Add create and update functions to message handler

--- handlers/messages.js
+++ handlers/messages.js
@@ -2,9 +2,42 @@ module.exports = function(db) {
 
   const MessageModel = require('../models/message')(db);
 
-  return {
+  const messageHandler = {
     loadAllMessages: async () => {
       return await db.loadMultiple('Message', {}, {});
+    },
+
+    createMessage: async messageProps => {
+      const newMessage = db.create('Message');
+      newMessage.setName(messageProps.name);
+      newMessage.setText(messageProps.text);
+      newMessage.compile();
+      await db.save('Message', newMessage);
+      return newMessage;
+    },
+
+    updateMessage: async messageProps => {
+      try {
+        const message = await db.load('Message', {name: messageProps.name});
+        message.setText(messageProps.text);
+        await db.save('Message', message);
+        return message;
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    createOrUpdateMessage: async messageProps => {
+      try {
+        return await messageHandler.updateMessage(messageProps);
+      } catch(error) {
+        if (error.name === 'NotFoundError') {
+          return await messageHandler.createMessage(messageProps);
+        }
+        throw error;
+      }
     }
   }
+
+  return messageHandler;
 }
